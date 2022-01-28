0d8bb8d1c99107a3f6e8b630e050d6c4ff39f727
Songchangyeop <waitwait0301@naver.com>
2022-01-28 16:02:15 +0900

    #15 feat: router 세팅 및 파일 구조 적용

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import MainPage from './Pages/MainPage/MainPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter basename="/pages">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
