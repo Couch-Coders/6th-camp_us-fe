05140ba45c4850024f4ca86fcdc16a4d40e9723d
Songchangyeop <waitwait0301@naver.com>
2022-01-28 16:37:07 +0900

    #15 feat: styled-components 설치 및 Global Style 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './routes';
+import GlobalStyle from './Styles/GrobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import MainPage from './Pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter basename="/pages">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter basename="/pages">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
