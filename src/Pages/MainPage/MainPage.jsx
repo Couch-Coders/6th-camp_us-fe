2fea08235aa6defdd50fb2b0863582499c3ed9df
Songchangyeop <waitwait0301@naver.com>
2022-01-29 21:08:08 +0900

    #5 feat:상세페이지 주요 정보 Table UI 추가

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,32 +1,31 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CampInfoTag from '../../Components/CampInfoTag/CampInfoTag';
+import * as campService from '../../Service/camps';
 
-const MainPage = (props) => {
+const MainPage = () => {
   const [campData, setCampData] = useState();
   const [campInfo, setCampInfo] = useState([]);
+  const [campReview, setCampReview] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   async function getCampData() {
-    try {
-      setIsLoading(true);
-      const response = await axios('http://localhost:3002/camp');
-      const data = response.data;
-      setCampData(data);
-      const info = data.sbrsCl.split(',');
-      setCampInfo(info);
-      setIsLoading(false);
-    } catch (error) {
-      throw new Error('error !!');
-    }
+    setIsLoading(true);
+    const response = await campService.getCamp();
+    const info = response.sbrsCl.split(',');
+    setCampData(response);
+    setCampInfo(info);
+    setIsLoading(false);
   }
 
-  console.log(campData);
-  console.log(campInfo);
+  async function getCampReview() {
+    const response = await campService.getReview();
+    setCampReview(response);
+  }
 
   useEffect(() => {
     getCampData();
+    getCampReview();
   }, []);
 
   return (
@@ -54,10 +53,43 @@ const MainPage = (props) => {
             </LikeWrap>
           </Header>
           <CampInfoWrap>
-            {campInfo.map((item) => (
-              <CampInfoTag tag={item} />
+            {campInfo.map((item, index) => (
+              <CampInfoTag tag={item} key={index} />
             ))}
           </CampInfoWrap>
+          <Thumbnail src={campData.firstImageUrl} alt="thumbnail"></Thumbnail>
+          <Table>
+            <tr>
+              <Th>주소</Th>
+              <Td>{campData.addr1}</Td>
+            </tr>
+            <tr>
+              <Th>문의처</Th>
+              <Td>{campData.tel}</Td>
+            </tr>
+            <tr>
+              <Th>운영일</Th>
+              <Td>{campData.operDeCl}</Td>
+            </tr>
+            <tr>
+              <Th>홈페이지</Th>
+              <Td>
+                <a href={campData.resveUrl}>{campData.resveUrl}</a>
+              </Td>
+            </tr>
+            <tr>
+              <Th>태그</Th>
+              <Td>{campData.posblFcltyCl}</Td>
+            </tr>
+            <tr>
+              <Th>주요시설</Th>
+              <Td>{campData.induty}</Td>
+            </tr>
+            <tr>
+              {campReview && <Th>별점 {campReview[0].contents[0].rate}</Th>}
+              <Td></Td>
+            </tr>
+          </Table>
         </Container>
       )}
     </Main>
@@ -121,4 +153,36 @@ const CampInfoWrap = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
+`;
+
+const Thumbnail = styled.img`
+  width: 100%;
+  margin-top: 25px;
+  margin-bottom: 37px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+`;
+
+const Th = styled.th`
+  display: flex;
+  justify-content: flex-start;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  color: #000000;
+`;
+
+const Td = styled.td`
+  width: 80%;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  color: #000000;
+  padding-bottom: 18px;
 `;
