05fa4a45d57931298190806932c10c1c5fdbaa54
hagaaDev <tjsgml951012@gmail.com>
2022-02-01 04:32:34 +0900

    feat: 메인페이지 검색 UI

--- src/Components/Main/MainSearch/MainSearch.js
+++ src/Components/Main/MainSearch/MainSearch.js
@@ -1,71 +1,91 @@
-import React from 'react';
-import { sido, sigungu } from './AddressData'
-import { Menu, Dropdown, Button, message, Input, Space, Form, Select } from 'antd';
+import React, { useState } from 'react';
+import * as district from './AddressData';
+import { Input, Space, Select } from 'antd';
 import 'antd/dist/antd.css';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-import { Section, InnerWrapper, SectionTitle } from '../MainPage.styles';
-import { SearchBox, /* Select, */ SelectName } from './MainSearch.styles';
+import { Section, InnerWrapper, SectionTitle } from '../../../Styles/theme';
+import { SearchBox, SelectName, SearchContent } from './MainSearch.styles';
 
 const { Option } = Select;
-  
-  function MainSearch (props) {
-    const onSearch = value => console.log(value);
-    const { Search } = Input;
-    const { address1, address2, setAddress1, setAddress2 } = props
-    
-    const changeAddress1 = (value) => {
-      setAddress1(value);
-      setAddress2(null);
-    }
 
-    const changeAddress2 = (value) => {
-      setAddress2(value);
-    }
-  
-    function handleMenuClick(e) {
-      // message.info('Click on menu item.');
-      console.log('click', e);
-    }
-  
-    /* const sidoList = (
-      <Menu onChange={changeAddress1} value={address1} onClick={handleMenuClick}>
-        {sido.map(s => (
-            <Menu.Item key={s} value={s}>{s}</Menu.Item>
-        ))}
-      </Menu>
-    );
+function MainSearch(props) {
+  /* 지역 검색 */
+  const sido = district.sido;
+  const sigungu = district.sigungu;
 
-    const sigunguList = (
-      {address1 ?
-        Object.keys(sigungu[address1]).sort().map(s => (
-          <Menu.Item key={s} value={s}>{s}</Menu.Item>
-      ))
-      : <></>
-      }
-    ); */
+  const [address1, setAddress1] = useState('서울특별시');
+  const [address2, setAddress2] = useState('강남구');
 
-      return (
+  const changeValue = (value) => {
+    setAddress1(value);
+    setAddress2(null);
+  };
+
+  const changeValue2 = (value) => {
+    setAddress2(value);
+  };
+
+  /* 별점 검색 */
+  const rateSelectList = [
+    '전체',
+    '1점 이상',
+    '2점 이상',
+    '3점 이상',
+    '4점 이상',
+    '5점 이상',
+  ];
+  const [RateSelected, setRateSelected] = useState('전체');
+
+  const changeValue3 = (value) => {
+    setRateSelected(value);
+  };
+
+  /* 캠핑장 이름 검색 */
+  const onSearch = (value) => console.log(value);
+  const { Search } = Input;
+
+  return (
     <Section grayBg>
       <InnerWrapper>
-          <SectionTitle>캠핑장 찾아보기</SectionTitle>
-          <SearchBox>
-          <Form.Item>
-                <Select placeholder="시/도" onChange={changeAddress1} value={address1}>
-                    {sido.map(s => (
-                        <Option key={s} value={s}>{s}</Option>
-                    ))}
-                </Select>
-            </Form.Item>
-            <Form.Item>
-                <Select placeholder="시/군/구" onChange={changeAddress2} value={address2}>
-                    {address1 ?
-                    Object.keys(sigungu[address1]).sort().map(s => (
-                        <Option key={s} value={s}>{s}</Option>
-                    ))
-                    : <></>
-                    }
-                </Select>
-            </Form.Item>
+        <SectionTitle>캠핑장 찾아보기</SectionTitle>
+        <SearchBox>
+          <Space wrap>
+            <SelectName>지역</SelectName>
+            <Select placeholder="시/도" onChange={changeValue} value={address1}>
+              {sido.map((s) => (
+                <Option key={s} value={s}>
+                  {s}
+                </Option>
+              ))}
+            </Select>
+            <Select
+              placeholder="시/군/구"
+              onChange={changeValue2}
+              value={address2}
+            >
+              {address1 ? (
+                sigungu[address1].map((s) => (
+                  <Option key={s} value={s}>
+                    {s}
+                  </Option>
+                ))
+              ) : (
+                <></>
+              )}
+            </Select>
+            <SelectName>별점</SelectName>
+            <Select
+              placeholder="별점"
+              onChange={changeValue3}
+              value={RateSelected}
+            >
+              {rateSelectList.map((s) => (
+                <Option key={s} value={s}>
+                  {s}
+                </Option>
+              ))}
+            </Select>
+          </Space>
+          <SearchContent>
             <Search
               placeholder="캠핑장 이름을 입력하세요."
               allowClear
@@ -73,10 +93,11 @@ const { Option } = Select;
               size="large"
               onSearch={onSearch}
             />
-          </SearchBox>
+          </SearchContent>
+        </SearchBox>
       </InnerWrapper>
     </Section>
-      );
-  }
-  
-  export default MainSearch;
+  );
+}
+
+export default MainSearch;
--- src/Components/Main/MainSearch/MainSearch.styles.js
+++ src/Components/Main/MainSearch/MainSearch.styles.js
@@ -1,23 +1,30 @@
 import styled from 'styled-components';
 
 export const SearchBox = styled.div`
-    position: relative;
-`;
+  position: relative;
 
-export const Select = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    flex-wrap: wrap;
-    margin-bottom: 10px;
+  @media screen and (max-width: 400px) {
+    & > * {
+      display: block;
+    }
+    & div {
+      display: block;
+      margin-top: 10px;
+    }
+  }
 `;
 
 export const SelectName = styled.div`
-    color: #595959;
-    font-size: 14px;
-    margin: 0 10px;
+  color: #595959;
+  font-size: 14px;
+  margin: 0 10px;
 
-    &:first-child {
-        margin-left: 0;
-    }
+  &:first-child {
+    margin-left: 0;
+  }
+`;
+
+export const SearchContent = styled.div`
+  max-width: 100%;
+  margin-top: 10px;
 `;
