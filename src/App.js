9eb4a2d0e95b6772086175034f76e71ef1fb9486
Songchangyeop <waitwait0301@naver.com>
2022-02-02 00:36:03 +0900

    #18 feat: firebase Google OAuth 인증 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DetailPage from './Pages/DetailPage/DetailPage';
 import MainPage from './Pages/MainPage/MainPage';
+import Navbar from './Components/Navbar/Navbar';
+import { AuthProvider } from './Components/auth/AuthProvider';
 
 function App() {
   return (
-    <BrowserRouter basename="/pages">
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/detail" element={<DetailPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter basename="/pages">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
