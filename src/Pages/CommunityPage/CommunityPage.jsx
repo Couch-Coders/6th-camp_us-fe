0ac6ca195902961be256ece23f710841fb984938
Songchangyeop <waitwait0301@naver.com>
2022-02-24 19:23:16 +0900

    #85 feat: 커뮤니티 페이지 이동시 개발 메시지, 메인 페이지 이동 버튼 추가

--- src/Pages/CommunityPage/CommunityPage.jsx
+++ src/Pages/CommunityPage/CommunityPage.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Result, Button } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 const CommunityPage = (props) => {
-  return <div>커뮤니티</div>;
+  const navigate = useNavigate();
+
+  const moveToMainPage = () => {
+    navigate('/');
+  };
+
+  return (
+    <Container>
+      <Result
+        icon={<SmileOutlined style={{ color: '#73d13d' }} />}
+        title="커뮤니티는 개발중입니다 조금만 기다려 주세요 !"
+        extra={
+          <Button
+            onClick={moveToMainPage}
+            type="primary"
+            style={{ backgroundColor: '#73d13d', border: '#73d13d' }}
+          >
+            메인페이지 이동
+          </Button>
+        }
+      />
+    </Container>
+  );
 };
 
 export default CommunityPage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
