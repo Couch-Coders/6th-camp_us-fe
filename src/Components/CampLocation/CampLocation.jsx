ba4ff44bb389ef642831d24978377218975ea805
Songchangyeop <waitwait0301@naver.com>
2022-02-27 03:19:06 +0900

    feat: 디테일페이지 마커 이미지 변경

--- src/Components/CampLocation/CampLocation.jsx
+++ src/Components/CampLocation/CampLocation.jsx
@@ -1,41 +1,54 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
+import { MapMarker, Map } from 'react-kakao-maps-sdk';
 import { CampContext } from '../../context/CampContext';
 import { style } from './CampLocation.style';
+import markerImg from '../../Assets/Images/marker.webp';
 
 const CampLocation = () => {
   const data = useContext(CampContext);
 
-  useEffect(() => {
-    let mapContainer = document.getElementById('map'), // 지도를 표시할 div
-      mapOption = {
-        center: new window.kakao.maps.LatLng(data.mapY, data.mapX), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-      };
-    let map = new window.kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-    // 마커가 표시될 위치입니다
-    let markerPosition = new window.kakao.maps.LatLng(data.mapY, data.mapX);
-
-    // 마커를 생성합니다
-    let marker = new window.kakao.maps.Marker({
-      position: markerPosition,
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-
-    // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-    // marker.setMap(null);
-  }, []);
-
   return (
     <section>
       <Title>찾아오시는 길</Title>
-      <Map id="map" />
+
+      <Map // 지도를 표시할 Container
+        center={{
+          // 지도의 중심좌표
+          lat: data.mapY,
+          lng: data.mapX,
+        }}
+        style={{
+          // 지도의 크기
+          width: '100%',
+          height: '450px',
+        }}
+        level={4} // 지도의 확대 레벨
+      >
+        <MapMarker // 마커를 생성합니다
+          position={{
+            // 마커가 표시될 위치입니다
+            lat: data.mapY,
+            lng: data.mapX,
+          }}
+          image={{
+            src: markerImg,
+            size: {
+              width: 60,
+              height: 70,
+            }, // 마커이미지의 크기입니다
+            options: {
+              offset: {
+                x: 27,
+                y: 69,
+              }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+            },
+          }}
+        />
+      </Map>
     </section>
   );
 };
 
 export default CampLocation;
 
-const { Title, Map } = style;
+const { Title } = style;
