a1389219a3268a17bb34017884a8ba0fdfb6c09f
Songchangyeop <waitwait0301@naver.com>
2022-03-07 02:59:41 +0900

    #112 refactor: 메인페이지 footer 추가

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -4,6 +4,7 @@ import MainCampRecommend from './mainCampRecommend/MainCampRecommend';
 import MainBestReview from './mainBestReview/MainBestReview';
 import styled from 'styled-components';
 import bannerImg from '../../assets/images/banner.webp';
+import logo from '../../assets/images/Logo.svg';
 
 const MainPage = () => {
   return (
@@ -14,6 +15,13 @@ const MainPage = () => {
       <MainSearch />
       <MainCampRecommend />
       <MainBestReview />
+      <MainFooter>
+        <FooterLogo />
+        <p>
+          <b>CampUs</b>
+        </p>
+        <p>© 2022 CampUs All rights reserved</p>
+      </MainFooter>
     </>
   );
 };
@@ -41,4 +49,34 @@ const Section = styled.section`
 const Banner = styled.img`
   width: 100%;
   height: auto;
+`;
+
+const MainFooter = styled.footer`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  font-size: 16px;
+  background-color: #f9f9f9;
+
+  @media screen and (max-width: 960px) {
+    font-size: 13px;
+  }
+`;
+
+const FooterLogo = styled.div`
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  background: url(${logo}) no-repeat center center / 100%;
+  top: 30px;
+  left: 50px;
+
+  @media screen and (max-width: 960px) {
+    top: 10px;
+    left: 10px;
+  }
 `;
