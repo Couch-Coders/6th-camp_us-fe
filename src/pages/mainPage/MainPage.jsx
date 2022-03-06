f041df3d7f62a292aca04af58e679140445a92bd
Songchangyeop <waitwait0301@naver.com>
2022-03-06 23:28:21 +0900

    #112 refactor: 메인페이지 배너 추가

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import MainSearch from './mainSearch/MainSearch';
 import MainCampRecommend from './mainCampRecommend/MainCampRecommend';
 import MainBestReview from './mainBestReview/MainBestReview';
+import styled from 'styled-components';
+import bannerImg from '../../assets/images/banner.webp';
 
 const MainPage = () => {
   return (
     <>
+      <Section>
+        <Banner src={bannerImg} alt="banner" />
+      </Section>
       <MainSearch />
       <MainCampRecommend />
       <MainBestReview />
@@ -14,3 +19,26 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
+const Section = styled.section`
+  width: 770px;
+  height: 385px;
+  margin: 0 auto;
+
+  @media screen and (max-width: 760px) {
+    width: 100%;
+    padding: 20px;
+    height: auto;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    padding: 20px;
+    height: 210px;
+  }
+`;
+
+const Banner = styled.img`
+  width: 100%;
+  height: auto;
+`;
