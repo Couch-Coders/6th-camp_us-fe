632a63e5a9df2f8f7b1c09b444a596ee2dbd9ad6
Songchangyeop <waitwait0301@naver.com>
2022-03-09 01:08:23 +0900

    #115 refactor: 게시글 CommunityPost 컴포넌트로 분리

--- src/pages/communityPage/category/CommunityAllPost.js
+++ src/pages/communityPage/category/CommunityAllPost.js
@@ -1,7 +1,7 @@
 import Avatar from 'antd/lib/avatar/avatar';
 import React from 'react';
+import CommunityPost from '../../../components/communityPost/CommunityPost';
 import { style } from './CommunityCategory.style';
-import { MessageFilled } from '@ant-design/icons';
 
 const CommunityAllPost = () => {
   return (
@@ -15,79 +15,11 @@ const CommunityAllPost = () => {
           <BestPostContent>내용</BestPostContent>
         </BestPosts>
       </BestPost>
-      <PostList>
-        <Post>
-          <PostType>캠퍼수다</PostType>
-          <PostTop>
-            <PostTitle>캠핑장 꿀팁</PostTitle>
-            <PostCreateTime>3시간전</PostCreateTime>
-          </PostTop>
-          <PostUserSet>
-            <PostUser>
-              <AvatarImg>
-                <img src="" alt="회원이미지" />
-              </AvatarImg>
-              <Nickname>닉네임</Nickname>
-            </PostUser>
-            <HandleContent>
-              <HandleReview>수정</HandleReview>
-              <HandleReview>삭제</HandleReview>
-            </HandleContent>
-          </PostUserSet>
-          <PostContent>
-            내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
-          </PostContent>
-          <PostImg>이미지 슬라이드 영역</PostImg>
-          <PostReact>
-            <LikeWrap>
-              <Like>
-                <LikeIcon
-                  viewBox="0 0 22 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M16.1817 0C13.923 0 11.964 1.32942 11 3.27142C10.036 1.32942 8.07697 0 5.81826 0C2.60477 0 0 2.69143 0 6.01173C0 12.5676 11 20 11 20C11 20 22 12.5676 22 6.01173C22 2.69143 19.3952 0 16.1817 0Z" />
-                </LikeIcon>
-              </Like>
-              <LikeCount>10</LikeCount>
-            </LikeWrap>
-            <CommentWrap>
-              <MessageFilled />
-              10
-            </CommentWrap>
-          </PostReact>
-        </Post>
-      </PostList>
+      <CommunityPost />
     </PostWrap>
   );
 };
 
 export default CommunityAllPost;
-const {
-  PostWrap,
-  Title,
-  BestPost,
-  BestPostTitle,
-  BestPosts,
-  BestPostContent,
-  PostList,
-  Post,
-  PostType,
-  PostTop,
-  PostTitle,
-  PostCreateTime,
-  PostUserSet,
-  PostUser,
-  AvatarImg,
-  Nickname,
-  HandleContent,
-  HandleReview,
-  PostContent,
-  PostImg,
-  PostReact,
-  LikeWrap,
-  Like,
-  LikeIcon,
-  LikeCount,
-  CommentWrap,
-} = style;
+const { PostWrap, Title, BestPost, BestPostTitle, BestPosts, BestPostContent } =
+  style;
