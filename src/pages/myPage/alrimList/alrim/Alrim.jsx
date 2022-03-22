e619e980c5357bf0cfdda3f01e75897321ef2099
김선희 <tjsgml951012@gmail.com>
2022-03-22 23:26:00 +0900

    #118 feat : 마이페이지 알림타입별 표현되는 데이터구분하여 표시

--- src/pages/myPage/alrimList/alrim/Alrim.jsx
+++ src/pages/myPage/alrimList/alrim/Alrim.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as api from '../../../../service/api';
 import { CloseOutlined } from '@ant-design/icons';
 import Image from '../../../../assets/images/default.png';
@@ -21,32 +21,96 @@ export default function Alrim({ alrim, MemberAlrimRequest }) {
     setRead(true);
   };
 
+  const [sliceText, setSliceText] = useState('');
+
+  // notificationType에 따라 sliceText에 들어갈 컨텐츠 내용 또는 제목 구분
+  useEffect(() => {
+    let notiContent = '';
+    if (alrim.notificationType !== 'postLike' && alrim.content.length > 10) {
+      notiContent = alrim.content.substring(0, 9) + `...`;
+      setSliceText(notiContent);
+    } else if (alrim.notificationType === 'postLike') {
+      notiContent = alrim.title;
+      if (alrim.title.length > 10)
+        notiContent = alrim.title.substring(0, 9) + `...`;
+      setSliceText(notiContent);
+    } else {
+      setSliceText(alrim.content);
+    }
+  }, [alrim]);
+
+  // notificationType에 따른 알림타입, 메세지, url, 썸네일이미지 구분
+  let notiType = '';
+  let notiMsg = '';
+  let notiURL = '';
+  let notiImg = '';
+
+  switch (alrim.notificationType) {
+    case 'postLike':
+      notiType = '게시글 좋아요';
+      notiMsg = '님이 회원님이 작성한 게시글을 좋아합니다';
+      notiURL = `/community/detail/?postId=${alrim.postId}`;
+      notiImg =
+        alrim.postImgUrlList.length === 0 ? Image : alrim.postImgUrlList[0];
+      break;
+
+    case 'commentLike':
+      notiType = '댓글 좋아요';
+      notiMsg = `님이 '${alrim.postTitle}' 게시글에 작성한 회원님의 댓글을 좋아합니다.`;
+      notiURL = `/community/detail/?postId=${alrim.postId}`;
+      notiImg =
+        alrim.postImgUrlList.length === 0 ? Image : alrim.postImgUrlList[0];
+      break;
+
+    case 'reviewLike':
+      notiType = '리뷰 좋아요';
+      notiMsg = `님이 회원님이 '${alrim.facltNm}'에 작성한 리뷰를 좋아합니다.`;
+      notiURL = `/detail?id=${alrim.campId}`;
+      notiImg =
+        alrim.imgUrl === '' ||
+        alrim.imgUrl === null ||
+        alrim.imgUrl === undefined
+          ? Image
+          : alrim.imgUrl;
+      break;
+
+    case 'commentWrite':
+      notiType = '새 댓글';
+      notiMsg = `님이 '${alrim.postTitle}' 게시글에 댓글을 작성하였습니다.`;
+      notiURL = `/community/detail/?postId=${alrim.postId}`;
+      notiImg =
+        alrim.postImgUrlList.length === 0 ? Image : alrim.postImgUrlList[0];
+      break;
+
+    default:
+      notiType = '';
+      notiMsg = '';
+      notiURL = `/community`;
+      break;
+  }
+
   return (
     <AlrimWrap key={alrim.notificationId} checked={alrim.checked} read={read}>
       <CheckedArea
-        to={`/detail?id=${alrim.campId}`}
+        to={notiURL}
         onClick={() => handleOnUpdate(alrim.notificationId)}
       >
         <Thumbnail>
-          <Thumb
-            src={
-              alrim.imgUrl === '' || alrim.imgUrl === null
-                ? Image
-                : alrim.imgUrl
-            }
-          ></Thumb>
+          <Thumb src={notiImg}></Thumb>
         </Thumbnail>
         <Info>
           <TopArea>
             <AlrimInfo>
-              <Type>{/* {alrim.type} */} 좋아요</Type>
+              <Type>
+                {notiType} {/* 좋아요 */}
+              </Type>
               <AlrimContent>
-                <b>{alrim.nickname}</b> 님이 리뷰에 좋아요를 눌렀습니다.
+                <b>{alrim.nickname}</b> {notiMsg}
               </AlrimContent>
             </AlrimInfo>
           </TopArea>
           <BottomArea>
-            <AlrimFrom>{alrim.facltNm}</AlrimFrom>
+            <AlrimFrom>{sliceText}</AlrimFrom>
             <Date>{chargeTime}</Date>
           </BottomArea>
         </Info>
